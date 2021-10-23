074a92e775a0187e6271c738b98d1d27aabe1a05
Apil Acharya <connectwithapil@gmail.com>
2021-10-23 22:45:27 +0545

    initial

--- traversy_node/devcamper1/server.js
+++ traversy_node/devcamper1/server.js
@@ -1,10 +1,19 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
 
+// Route files 
+import bootcamps from './routes/bootcamps.js'
 // Load env vars
-dotenv.config({path:'./config/config.env'})
+dotenv.config({ path: './config/config.env' });
 
-const app = express()
+const app = express();
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use('/api/v1/bootcamps', bootcamps)
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
