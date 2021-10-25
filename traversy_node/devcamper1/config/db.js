7ad8df2c0931926d7a0f9832316f80c429384000
Apil Acharya <connectwithapil@gmail.com>
2021-10-25 19:12:06 +0545

    middle commit

--- traversy_node/devcamper1/config/db.js
+++ traversy_node/devcamper1/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async() => {
   const conn = await mongoose.connect(process.env.MONGO_URI)
-  console.log(`MongoDB Connected: ${conn.connection.host}`)
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
 }
 
 export default connectDB
--- traversy_node/devcamper1/server.js
+++ traversy_node/devcamper1/server.js
@@ -1,34 +1,41 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import morgan from 'morgan'
+import morgan from 'morgan';
+import colors from 'colors'
 
 // Route files
 import bootcamps from './routes/bootcamps.js';
 
 // DB file
-import connectDB from './config/db.js'
+import connectDB from './config/db.js';
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 // Activating database
-connectDB()
+connectDB();
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Dev logging middleware
-if(process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
-
 
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // Close server and end process
+  server.close(() => process.exit(1))
+})
